3c321681324e52bffdbd9b56b42494efa747faa6
michal.stehlik <michal.stehlik@PC2021-01>
2022-09-12 13:34:35 +0200

    Add project files.

---
+++ asp01database/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using asp01database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp01database.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Student>().HasData(new Student { StudentId = 1, Firstname = "Otmar", Lastname = "Drtina"});
+        }
+    }
+}
---
+++ asp01database/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace asp01database.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Firstname { get; set; } = String.Empty;
+        public string Lastname { get; set; } = String.Empty;
+    }
+}
