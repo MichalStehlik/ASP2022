1d4c55ed1cccedd0acb8f47a9fc8be87ec09b58f
Michal Stehlik <michal.stehlik@pslib.cz>
2022-09-14 14:14:48 +0200

    fix

--- asp01database/Models/Student.cs
+++ asp01database/Models/Student.cs
@@ -4,7 +4,7 @@ namespace asp01database.Models
 {
     public class Student
     {
-        public int StudentId { get; set; }
+        public int StudentId { get; set; } // Id
         public string Firstname { get; set; } = String.Empty;
         public string Lastname { get; set; } = String.Empty;
     }
